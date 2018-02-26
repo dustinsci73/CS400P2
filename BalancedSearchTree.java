aae2031158c7ed0f757fd4d0e13aa5410f8977bb
Brennan <31396350+brenfife@users.noreply.github.com>
2018-02-26 12:33:37 -0600

    Update BalancedSearchTree.java

--- BalancedSearchTree.java
+++ BalancedSearchTree.java
@@ -1,19 +1,43 @@
-// starter class for a BalancedSearchTree
-// you may implement AVL, Red-Black, 2-3 Tree, or 2-3-4 Tree
-// be sure to include in class header which tree you have implemented
+///////////////////////////////////////////////////////////////////////////////
+// Title:            P2 (Red-Black Tree)
+// Files:            BalancedSearchTree.java, SearchTreeADT.java, 
+//		     TestSearchTree.java
+// Semester:         CS 400 Spring 2018
+//
+// Author:           (your name)
+// Email:            (your email address)
+// Lecturer's Name:  Deb Deppeler
+//
+// Pair Partner:     (name of your pair programming partner)
+// Email:            (email address of your programming partner)
+// Known Bugs:	      None
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * The program makes use of a RED-BLACK TREE. Makes use of the given skeleton
+ * code.
+ *
+ * @author (your name)
+ */
 public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeADT<T> {
 
-	// inner node class used to store key items and links to other nodes
+	/**
+	 * Class for nodes in the binary tree. Stores key items and links to 
+	 * other nodes
+	 * @author (your name)
+	 * @param <K> generic comparable object for key
+	 */
 	protected class Treenode<K extends Comparable<K>> {
 		public Treenode(K item) {
 			this(item,null,null,null,false);
 		}
 		public Treenode(K item, Treenode<K> left, Treenode<K> right, Treenode<K> parent, boolean color) {
 			key = item;
-			this.left = left;
-			this.right = right;
-			this.color = color;
-			this.parent = parent;
+			this.left = left; //left child
+			this.right = right; //right child
+			this.color = color; //node color
+			this.parent = parent; //node parent
 		}
 		
 		boolean color;
@@ -26,9 +50,13 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
     private static final boolean RED   = true;
     private static final boolean BLACK = false;
 	protected Treenode<T> root;
-
+	
+	/**
+	 * Used to call AscendingOrderHelper.
+	 *
+	 * @return AscendingOrderHelper call to the helper AscendingOrderHelper
+	 */
 	public String inAscendingOrder() {
-		//TODO : must return comma separated list of keys in ascending order
 		return AscendingOrderHelper(root);
 	}
 	
@@ -54,16 +82,26 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		}
 	}
 
+	/**
+	 * Returns true if our tree is empty
+	 */
 	public boolean isEmpty() {
-		//TODO return empty if there are no keys in structure
 		return root == null;
 	}
 
+	/**
+	 * Returns the height of our tree
+	 */
 	public int height() {
-		//TODO return the height of this tree
 		return heightHelper(root); 
 	}
 	
+	/**
+	 * Returns the height of our subtree.
+	 *
+	 * @param node Where we are starting the count on height
+	 * @return The height of our subtree 
+	 */
 	private int heightHelper(Treenode<T> node)
 	{
 		if (node == null)
@@ -76,8 +114,12 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		}
 	}
 
+	/**
+	 * Returns true if the tree contains a node with the item in question
+	 *
+	 * @param item The item we are looking up.
+	 */
 	public boolean lookup(T item) {
-		//TODO must return true if item is in tree, otherwise false
 		if (item == null)
 		{
 			throw new IllegalArgumentException();
@@ -88,6 +130,13 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		}
 	}
 	
+	/**
+	 * Given the node, are trying to determine if the value is within our tree.
+	 * If it is, then return true.
+	 *
+	 * @param node The node to begin off of
+	 * @param item What value is contained within the node
+	 */
 	private boolean lookupHelper(Treenode<T> node, T item)
 	{
 		if (node == null)
@@ -109,6 +158,15 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		
 	}
 
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	public void insert(T item) {
 		//TODO if item is null throw IllegalArgumentException, 
 		// otherwise insert into balanced search tree
@@ -130,6 +188,16 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		}
 	}
 
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param item (Describe the first parameter here)
+	 * @param node (Do the same for each additional parameter)
+	 * @return newTreeNode 
+	 * @return insertHelper
+	 */
 	private Treenode<T> insertHelper(T item, Treenode <T> node)
 	{
 		if (item.compareTo(node.key) > 0)
@@ -162,6 +230,16 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 			}
 		}
 	}
+	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	public void delete(T item) {
 		//TODO if item is null or not found in tree, return without error
 		// else remove this item key from the tree and rebalance
@@ -178,6 +256,15 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		}
 	}
 	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	private Treenode<T> deleteHelper(T item, Treenode<T> node)
 	{
 		if (item.compareTo(node.key) > 0)
@@ -215,10 +302,29 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		return node;
 	}
 	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	private void balance(Treenode<T> node)
 	{
 		
 	}
+	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	private void rotateLeft(Treenode<T> node)
 	{
 		Treenode<T> temp;
@@ -247,6 +353,16 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		temp.left = node;
 		node.parent = temp;
 	}
+	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	private void rotateRight(Treenode<T> node)
 	{
 		Treenode<T> temp;
@@ -275,16 +391,19 @@ public class BalancedSearchTree<T extends Comparable<T>> implements SearchTreeAD
 		temp.right = node;
 		node.parent = temp;
 	}
-
-
-	// HINT: define this helper method that can find the smallest key 
-	// in a sub-tree with "node" as its root
-	// PRE-CONDITION: node is not null
+	
+	/**
+	 * (Write a succinct description of this method here.  If necessary,
+	 * additional paragraphs should be preceded by <p>, the html tag for
+	 * a new paragraph.)
+	 *
+	 * @param (parameter name) (Describe the first parameter here)
+	 * @param (parameter name) (Do the same for each additional parameter)
+	 * @return (description of the return value)
+	 */
 	private T leftMost(Treenode<T> node) {
 		// TODO return the key value of the left most node in this subtree
 		// or return node's key if node does not have a left child
 		return node.key;
 	}
-
 }
-
